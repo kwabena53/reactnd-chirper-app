5205e61fd8cf990450ec640ff17c1dd44b0207b2
kwabena.adu-darkwa <kwabena.adu-darkwa@makeduconsult.com>
2020-05-26 06:30:10 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
+import LoadingBar from 'react-redux-loading'
+import TweetPage from './TweetPage'
+
+
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  
   render() {
     return (
       <div>
-        Starter Code
-      </div>
-    )
+        <LoadingBar />
+      {this.props.loading === true
+        ? null
+        : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>}
+
+    </div>  
   }
 }
 
-export default App
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
